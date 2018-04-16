e46be55d29bbad79beed9bb0b3b84fef9bd4e406
robertmills1985 <robertmills1985@yahoo.com>
2018-04-15 23:39:36 -0700

    succesfully completed alk component and completed other readings component. Still needs a comment box but then thats it. I can move on to services provided components

--- src/OtherReadings.js
+++ src/OtherReadings.js
@@ -18,6 +18,36 @@ class OtherReadings extends Component {
             salt:'Salt is at ' + salt
         })
     }
+    handleTemp(){
+        var temp= document.getElementById('temp-reading').value 
+        this.setState({
+            temp:'Tempature is at ' + temp
+        })
+    }
+    handlePhos(){
+        var phos= document.getElementById('phos-reading').value 
+        this.setState({
+            phos:'Phos is at ' + phos
+        })
+    }
+    handleCalc(){
+        var calc= document.getElementById('calc-reading').value 
+        this.setState({
+            calc:'Calcium is at ' + calc
+        })
+    }
+    handleCya(){
+        var cya= document.getElementById('cya-reading').value 
+        this.setState({
+            cya:'Cyanuric is at ' + cya
+        })
+    }
+    handlePres(){
+        var pres= document.getElementById('pres-reading').value 
+        this.setState({
+            pres:'Filter pressure is at ' + pres
+        })
+    }
 	render() {
 		return (
 			<div class="chem-panel">
@@ -27,27 +57,27 @@ class OtherReadings extends Component {
 					<input id="salt-reading" type="text" placeholder="Salinity" />
 					<button onClick={()=>this.handleSalt()}>Save</button>
 					<input id="temp-reading" type="text" placeholder="Teampature" />
-					<button  >Save</button>
+					<button onClick={()=>this.handleTemp()} >Save</button>
 					<input id="phos-reading" type="text" placeholder="Phospahtes" />
-					<button>Save</button>
+					<button onClick={()=>this.handlePhos()} >Save</button>
 					<input id="calc-reading" type="text" placeholder="Calcium" />
-					<button>Save</button>
+					<button onClick={()=>this.handleCalc()} >Save</button>
 					<input id="cya-reading" type="text" placeholder="Cyanuric" />
-					<button>Save</button>
+					<button onClick={()=>this.handleCya()} >Save</button>
 				</div>
 				<div>
 					<input id="pres-reading" type="text" placeholder="Pressure" />
-					<button>Save</button>
+					<button onClick={()=>this.handlePres()} >Save</button>
 				</div>
 				<div>
 					<h4>Readings</h4>
-					<p>{this.state.tds}</p>
-					<p>{this.state.salt}</p>
-					<p>{this.state.temp}</p>
-					<p>{this.state.phos}</p>
-					<p>{this.state.calc}</p>
-					<p>{this.state.cya}</p>
-					<p>{this.state.pres}</p>
+					<p id='tds-readings' >{this.state.tds}</p>
+					<p id='salt-readings' >{this.state.salt}</p>
+					<p id='temp-readings' >{this.state.temp}</p>
+					<p id='phos-readings' >{this.state.phos}</p>
+					<p id='calc-readings' >{this.state.calc}</p>
+					<p id='cya-readings' >{this.state.cya}</p>
+					<p id='pres-readings' >{this.state.pres}</p>
 				</div>
 			</div>
 		);
