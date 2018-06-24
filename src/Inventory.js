710df9b199358b8e2b2b1f99877474942f1059ae
robertmills1985 <robertmills1985@yahoo.com>
2018-06-24 12:12:44 -0700

    inventory component is now functioning. Needs some tweaking on app.js end but other than that its ready.

--- src/Inventory.js
+++ src/Inventory.js
@@ -6,11 +6,50 @@ class Inventory extends Component {
 
 		this.state = {};
 	}
+	handleInvSave() {
+		var tabs = document.getElementById('tabs-used').value;
+		var acid = document.getElementById('acid-used').value;
+		var bicarb = document.getElementById('bicarb-used').value;
+		var bisulfate = document.getElementById('bisulfate-used').value;
 
+		this.setState({
+			tabs:tabs + " tabs were added.",
+			acid:acid + " gallons of acid were added. ",
+			bicarb:bicarb + " lbs of bicarb. ",
+			bisulfate:bisulfate + " lbs of bisulfate were added. "
+		})
+		
+		
+	}
 	render() {
 		return (
 			<div class="sub-panel">
 				<h1>Inventory Used</h1>
+				<ul>
+					<li>
+						{' '}
+						<input id="tabs-used" type="text" placeholder="tabs" />
+					</li>
+					<li>
+						{' '}
+						<input id="acid-used" type="text" placeholder="acid" />
+					</li>
+					<li>
+						{' '}
+						<input id="bicarb-used" type="text" placeholder="bicarb" />
+					</li>
+					<li>
+						{' '}
+						<input id="bisulfate-used" type="text" placeholder="bisulfate" />
+					</li>
+				</ul>
+				<button onClick={() => this.handleInvSave()}>Save</button>
+				<div hidden>
+					<p id='tu' >{this.state.tabs}</p>
+					<p id='au' >{this.state.acid}</p>
+					<p id='cu' >{this.state.bicarb}</p>
+					<p id='su' >{this.state.bisulfate}</p>
+				</div>
 			</div>
 		);
 	}
